4f245559062d942c19a840ef8d46f5272532e9e6
Daniele Bacarella <daniele.bacarella@gmail.com>
2012-11-25 17:05:05 +0100

    Add city continent mapping , google map api dimissed

--- src/stream_elisa.py
+++ src/stream_elisa.py
@@ -3,15 +3,19 @@ from tweepy.streaming import StreamListener
 from googlemaps import GoogleMaps
 import auth_elisa
 import json
+import threading
+import time
 
+TOPIC = ''
 
-TOPIC = 'gaza'
-
+    
 class elisa_stream_listener(StreamListener):
 
-    DEBUG = True
+    DEBUG = False
     gmaps = None
-    countries_continent_json = None
+
+    countries_continent = {}
+    
     continents_counts = {'North America': 0,
                         'Asia': 0,
                         'Europe': 0,
@@ -21,6 +25,55 @@ class elisa_stream_listener(StreamListener):
                         }
 
     tweets_cnt = 0
+
+
+    def get_continent_from_city(self, city):
+        result= self.countries_continent[city.lower()]
+        if result != '' :
+            return result
+        else:
+            return 'unknown'
+        
+
+    def load_cities(self):
+        cities_fp = open("../resources/city_continent_map","r")
+        cities  = cities_fp.readlines()
+
+        for city in cities:
+            self.countries_continent.update({city.split("\t")[0].lower().strip() : city.split("\t")[2].strip()})
+            print {city.split("\t")[0].strip().lower() : city.split("\t")[2].strip()}
+        cities_fp.close()
+        
+            
+    def start_thread_print_stats(self):
+        t_print_stats = threading.Thread(target=self.timeout_print_stats)
+        t_print_stats.start()
+
+    def get_continent(self, continent):
+        tot_count= self.tweets_cnt
+        if tot_count == 0 :
+            return 0
+        else:
+            count = self.continents_counts[continent]
+            return self.trunc( float(count) / tot_count, 4)
+        
+    def timeout_print_stats(self):
+        while True:
+            try:
+                time.sleep(5)
+                print 'Topic: ', TOPIC
+                print 'North America: ',  self.get_continent('North America')
+                print 'South America: ', self.get_continent('South America')
+                print 'Europa: ',self.get_continent('Europe')
+                print 'Asia: ', self.get_continent('Asia')
+                print 'Africa: ',self.get_continent('Africa')
+                print 'Total Tweets: ', self.tweets_cnt
+                print '\n'
+            except Exception as E:
+                if self.DEBUG:
+                    print E
+            
+        
     
     def trunc(self,f, n):
         '''Truncates/pads a float f to n decimal places without rounding'''
@@ -33,64 +86,28 @@ class elisa_stream_listener(StreamListener):
         new_continent_count= self.continents_counts[continent]+1
         self.continents_counts[continent]=  new_continent_count
         
-        #self.continents_ratio['North America']= self.trunc( round( float(self.continents_ratio['North America']) / self.tweets_cnt,5), 5)
-        #self.continents_ratio['Asia']=  self.trunc( round( float(self.continents_ratio['Asia']) / self.tweets_cnt,5), 5)
-        #self.continents_ratio['Europe']=  self.trunc( round( float(self.continents_ratio['Europe']) / self.tweets_cnt,5), 5)
-        #self.continents_ratio['South America']=  self.trunc( round( float(self.continents_ratio['South America']) / self.tweets_cnt,5), 5)
-        #self.continents_ratio['Africa']=  self.trunc( round( float(self.continents_ratio['Africa']) / self.tweets_cnt,5), 5)
-        #self.continents_ratio[continent]=   self.trunc( round( ( float(tmp_continent_stat)+1) / self.tweets_cnt,5),5)
+      
         return
         
-    def auth_gmaps(self):
-        self.gmaps = GoogleMaps('48e7668b14812c0bed47256d2234e7620e5d030e')
-
-    def load_countries_continent_map(self):
-        countries_continent_json_file = open('Countries-Continents.json')
-        self.countries_continent_json = json.load(countries_continent_json_file)
-        countries_continent_json_file.close()
-
-    def get_complete_address(self,loc):
-        try:
-           
-            #Fetching continent
-            lat,lng = self.gmaps.address_to_latlng(loc)
-            location = self.gmaps.latlng_to_address(lat,lng)
-            start_index_country = location.rfind(',')
-            country = location[start_index_country+1:]
-            continent = self.countries_continent_json[country.strip()]
-            self.update_stats(continent)
-            return  continent
-            
-        except Exception as E:
-            if self.DEBUG:
-                print E
-            return 'unknown'
+        
 
     def on_error(self,status):
         print status
     
     def on_data(self,data):
-        #print data
-
         
         tweet_dict = json.loads(data)
         
         try:
             location = tweet_dict['user']['location']
-            continent = self.get_complete_address(location)
+            continent = self.get_continent_from_city(location)
             if continent != 'unknown':
-                print 'Topic: ', TOPIC
-                print 'North America: ',  self.trunc( float(self.continents_counts['North America'])/self.tweets_cnt , 4 )
-                print 'South America: ', self.trunc( float(self.continents_counts['South America'])/self.tweets_cnt, 4 )
-                print 'Europa: ', self.trunc( float(self.continents_counts['Europe'])/self.tweets_cnt, 4)
-                print 'Asia: ', self.trunc( float(self.continents_counts['Asia'])/self.tweets_cnt, 4)
-                print 'Africa: ', self.trunc( float(self.continents_counts['Africa'])/self.tweets_cnt, 4)
-                print 'Total Tweets: ', self.tweets_cnt
-                print '\n'
-                #print get_complete_address(long_address)
+                self.update_stats(continent)
+                
                 
         except Exception as E:
-            print E
+            if self.DEBUG:
+                print E
 
         return True
 
@@ -102,14 +119,15 @@ if __name__ == '__main__':
     print "Elisa:: authentication"
     
     l = elisa_stream_listener()
-    l.auth_gmaps()
-    l.load_countries_continent_map()
-    
+    l.load_cities()
+    l.start_thread_print_stats()
     auth_cons_key = auth_elisa.set_consumer_key()
     auth = auth_elisa.set_access_token(auth_cons_key)
     api = auth_elisa.get_API(auth)
     
     auth_elisa.print_name(api)
+
     
+   
     stream = Stream(auth, l)
     stream.filter(track=[TOPIC])
