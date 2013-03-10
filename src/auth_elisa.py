bab61458a6c20217e9660c619ece1adc398cc4df
Daniele Bacarella <daniele.bacarella@gmail.com>
2013-03-10 22:26:19 +0100

    Update auth_elisa.py

--- src/auth_elisa.py
+++ src/auth_elisa.py
@@ -3,14 +3,14 @@ from tweepy import OAuthHandler
 
 def set_consumer_key():
     #OAuth 
-    consumer_key = "yRUjyLvZUxIYlkCQdm2pQ"
-    consumer_secret = "zsbTIIuma2CLRIG9eq6zAYwHLdMw6x3WfaMBPKMhtSg"
+    consumer_key = ""
+    consumer_secret = ""
     Auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
     return Auth
 
 def set_access_token(auth):
-    access_token = "305859183-EnKfk6f00xHZ7k8AZRSUT1NYNZJwIb9WKyDCk2k"
-    access_token_secret = "C5oPHAcbWWrL9AZlWsNAtoS3I5rz7hXmnJgDxUg1M"
+    access_token = ""
+    access_token_secret = ""
     auth.set_access_token(access_token,access_token_secret)
     return auth
 
